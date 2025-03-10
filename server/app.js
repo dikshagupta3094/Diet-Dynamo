53b43b5be81ccc64ad2843bb80f44714fe2f5703
dikshagupta3094 <dikshagpragya@gmail.com>
2025-03-10 14:15:56 +0530

    fixed server configuration

--- server/app.js
+++ server/app.js
@@ -1,12 +1,9 @@
 import express from "express";
-import http from "http";
-// Create Server
 const app = express();
-const server = http.createServer(app);
 
 app.use(express.json());
 app.all("*", (req, res) => {
   return res.status(404).send("Oops! Page not found");
 });
 
-export { app, server };
+export default app;
