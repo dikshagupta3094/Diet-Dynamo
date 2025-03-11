7b37310805b4f19577f5c74bc13d2cf24d5ca42a
Anusha Sahu <anushasahu0507@gmail.com>
2025-03-11 15:17:32 +0530

    Implemented email verification using OTP

--- server/models/expert.model.js
+++ server/models/expert.model.js
@@ -5,19 +5,16 @@ const expertSchema = new mongoose.Schema({
     qualification:[
         {
             type: String,
-            required: true
         }
     ],
     description:{
         type: String,
-        required: true,
         min: [50, "Minimum 50 words are required to describe"],
         max: [1000, "Maximum word limit for description is 1000"],
         trim: true
     },
     degree:{
         type: String,
-        required: true,
     }
 })
 
