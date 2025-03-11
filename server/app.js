62261e4a9b2bad240112e34384aab097a099f676
dikshagupta3094 <dikshagpragya@gmail.com>
2025-03-11 15:15:24 +0530

    create user route

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 const app = express();
-
+import auth from "./routes/user.routes.js";
 app.use(express.json());
+
+app.use('/api/v1/auth',auth)
+
 app.all("*", (req, res) => {
   return res.status(404).send("Oops! Page not found");
 });
--- server/models/expert.model.js
+++ server/models/expert.model.js
@@ -1,23 +1,20 @@
 import mongoose from "mongoose";
-import User from "./user.model";
+import User from "./user.model.js";
 const expertSchema = new mongoose.Schema({
     
     qualification:[
         {
             type: String,
-            required: true
         }
     ],
     description:{
         type: String,
-        required: true,
         min: [50, "Minimum 50 words are required to describe"],
         max: [1000, "Maximum word limit for description is 1000"],
         trim: true
     },
     degree:{
-        type: String,
-        required: true,
+        type: String
     }
 })
 
