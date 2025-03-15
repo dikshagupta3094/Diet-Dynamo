33fb8cc1a2bfbe7fce42b2996b89b746ed73307d
dikshagupta3094 <dikshagpragya@gmail.com>
2025-03-15 20:51:33 +0530

    fix verifyEmail

--- server/routes/otp.routes.js
+++ server/routes/otp.routes.js
@@ -1,13 +1,8 @@
 import express from "express"
 import {sendOTP} from "../controllers/otp.controller.js"
-import { verifyEmail } from "../controllers/verifyEmail.controller.js";
-
 const router = express.Router();
 
 // send otp
 router.post('/send-otp', sendOTP);
-
-//verify otp
-router.post('/verify-otp', verifyEmail);
 
 export default router;
