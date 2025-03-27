e5d216e5f64d65e8dfe4428871a1a0f91f1b223c
Anusha Sahu <anushasahu0507@gmail.com>
2025-03-27 23:53:11 +0530

    added post query

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import HomeLayout from '../Layout/HomeLayout'
+import Banner from '../assets/Banner.jpg'
 const HomePage = () => {
   return (
     <HomeLayout>
-      Home Page
+      <div className="" style={{ backgroundImage: `url(${Banner})`, 
+        display:"flex", justifyItems:"center", alignItems:"center", backgroundPosition:"center", 
+        backgroundRepeat:"no-repeat", backgroundSize:"cover" }}>
+        <div className="">
+          <div className=''>Welcome to Diet Dynamo</div><br />
+          <h1>Nourish your body,<br /> love your life.</h1>
+          <p>Fuel your body with healthy choices and experience sustained energy <br />
+           for a life brimming with vitality.</p>
+          {/* <Link to="/contact">
+            <button>CONTACT US</button>
+          </Link> */}
+        </div>
+      </div>
     </HomeLayout>
   )
 }
